6909a200ca076a625e5c63db7d00960b547c55e5
juyeong <yp9782@gmail.com>
2024-10-11 16:13:24 +0900

    구글 폰트 적용, 파일 구분

---
+++ weather-app/src/pages/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../css/main.css';
+
+const Main = () => {
+    return(
+        <div className="container">
+            <div className="searchRegion">
+                <h3>원하는 지역을 검색하세요.</h3>
+                <input type="text" />        
+            </div>
+        </div>
+
+    )
+}
+
+export default Main;
