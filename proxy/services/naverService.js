dc8d9f2de0fb41cc226b4422c00226f5c42e6bb3
juyeong <yp9782@gmail.com>
2024-10-24 15:25:46 +0900

    서버 연동 위한 형태 변경( params 생성)

--- proxy/services/naverService.js
+++ proxy/services/naverService.js
@@ -4,9 +4,14 @@
 
 import fetch from 'node-fetch';
 import config from '../config/index.js';
+import axios from 'axios';
+import { query } from 'express';
 
 export const getGeocode = async (address) => {
     const encodedAddress = encodeURIComponent(address);
+    
+    try {
+        /*
     const response = await fetch(
         `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodedAddress}`,
         {
@@ -20,6 +25,23 @@ export const getGeocode = async (address) => {
     if (!response.ok) {
         throw new Error('Failed to fetch data from Naver API');
     }
-    
     return response.json();
+    */
+
+    const response = await axios.get ('https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode', {
+        params :{
+            query : encodedAddress
+        },
+        headers: {
+            'X-NCP-APIGW-API-KEY-ID': config.naver.id,
+            'X-NCP-APIGW-API-KEY': config.naver.key,
+        }
+    });
+    return response.json();
+
+    } catch (error){
+        console.error("geocodong API Error:", error);
+        throw error;
+    }
+
 };
--- proxy/services/weatherService.js
+++ proxy/services/weatherService.js
@@ -4,6 +4,7 @@
 
 import fetch from 'node-fetch';
 import config from '../config/index.js';
+import axios from 'axios';
 
 /**
  * 초단기 실황 : 오늘의 예보 가져오기
@@ -17,15 +18,32 @@ export const getWeatherData = async (base_date, base_time, nx, ny) => {
     const truncatedNx = Math.trunc(nx);
     const truncatedNy = Math.trunc(ny);
     
-    const response = await fetch(
-        `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${config.weather.key}&numOfRows=10&pageNo=1&base_date=${base_date}&base_time=${base_time}&nx=${truncatedNx}&ny=${truncatedNy}&dataType=JSON`
-    );
-
-    if (!response.ok) {
-        const errorText = await response.text(); // 응답 텍스트를 가져옴
-        console.error("Weather API Error:", errorText); // 오류 로그 출력
-        throw new Error('Failed to fetch weather data');
+    try {
+        /*
+        const response = await fetch(
+            `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${config.weather.key}&numOfRows=10&pageNo=1&base_date=${base_date}&base_time=${base_time}&nx=${truncatedNx}&ny=${truncatedNy}&dataType=JSON`
+        );
+        if (!response.ok) {
+            const errorText = await response.text(); // 응답 텍스트를 가져옴
+            console.error("Weather API Error:", errorText); // 오류 로그 출력
+            throw new Error('Failed to fetch weather data');
+        }
+        */
+        const response = await axios.get('`http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst', {
+            params : {
+                serviceKey: config.weather.key,
+                pageNo : 1,
+                numOfRows : 10,
+                dataType : 'JSON',
+                base_date,
+                base_time,
+                nx,
+                ny
+            }
+        });
+        return response.json();
+    } catch (error) {
+        console.error("Weather API Error:", error);
+        throw error;
     }
-
-    return response.json();
 };
