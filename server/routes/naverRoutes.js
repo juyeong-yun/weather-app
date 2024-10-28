95d6276cfc4a07cece87988fb9101f0b94c23bb5
juyeong <yp9782@gmail.com>
2024-10-28 18:11:21 +0900

    delete / 안쓰는 부분 삭제

--- server/routes/naverRoutes.js
+++ server/routes/naverRoutes.js
@@ -8,12 +8,6 @@ import { naverApiHandler } from '../controllers/naverController.js';
 
 const router = express.Router();
 
-router.get('/naver', (req, res) => {
-    const query = req.query.query; // 쿼리 파라미터 가져오기
-    // Naver API와의 통신 로직 작성
-    res.json({ message: `Naver query: ${query}` });
-});
-
 router.get('/', naverApiHandler);
 router.get('/kisangcheong-test', naverApiHandler);
 
