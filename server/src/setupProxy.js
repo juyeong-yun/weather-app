3f0efb64231fdf830094d1cc1819784a2bd33985
juyeong <yp9782@gmail.com>
2024-10-28 18:10:48 +0900

    Update / proxy 수정

--- server/src/setupProxy.js
+++ server/src/setupProxy.js
@@ -1,19 +1,10 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
-  // /weather-app/api 경로에 대한 프록시 설정
   app.use(
-    proxy('/weather-app/api', { 
-      target: 'http://localhost:4000/', // 통신할 서버의 도메인주소
+    proxy('/weather-app/api', {  //도메인 api로 호출
+      target: 'http://localhost:4000/', //통신할 서버의 도메인주소
       changeOrigin: true,
     })
-  );
-
-  // /weather-app/kisangcheong-test/api 경로에 대한 프록시 설정
-  app.use(
-    proxy('/weather-app/kisangcheong-test/api', { 
-      target: 'http://localhost:4000/', // 통신할 서버의 도메인주소
-      changeOrigin: true,
-    })
-  );
+  )
 }
