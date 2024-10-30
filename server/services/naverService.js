c2e40496ca14fca289cd6d0a3dcd3a6d64b89b40
juyeong <yp9782@gmail.com>
2024-10-30 17:44:15 +0900

    네이버 API 주소 전달 오류 해결

--- server/services/naverService.js
+++ server/services/naverService.js
@@ -1,6 +1,5 @@
-/**
- * 네이버 관련 
- */
+// ./services/naverService.js
+// 네이버 API 연결
 
 import fetch from 'node-fetch';
 import config from '../config/index.js';
@@ -8,7 +7,8 @@ import axios from 'axios';
 import { query } from 'express';
 
 export const getGeocode = async (address) => {
-    const encodedAddress = encodeURIComponent(address);
+    // const encodedAddress = encodeURIComponent(address);
+    // console.log(`encodedAddress: ${encodedAddress}, address : ${address}}`);
     
     try {
         /*
@@ -30,14 +30,17 @@ export const getGeocode = async (address) => {
 
     const response = await axios.get ('https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode', {
         params :{
-            query : encodedAddress
+            query : address
         },
         headers: {
             'X-NCP-APIGW-API-KEY-ID': config.naver.id,
             'X-NCP-APIGW-API-KEY': config.naver.key,
         }
     });
-    return response.json();
+    // response.data를 사용하여 JSON 데이터를 반환
+    // console.log("res data (JSON):", JSON.stringify(response.data, null, 2));
+    return response.data;
+
 
     } catch (error){
         console.error("geocodong API Error:", error);
