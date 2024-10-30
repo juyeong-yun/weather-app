7cb302de6baca6b12e3d407b182c80da00d5470e
juyeong <yp9782@gmail.com>
2024-10-30 17:47:27 +0900

    fetchWeatherData 파라미터 오타 수정

--- weather-app/src/services/apiService.js
+++ weather-app/src/services/apiService.js
@@ -7,15 +7,19 @@
  */
 export const fetchGeoData = async (address, isKisangcheongTest) => {
     const baseName = process.env.REACT_APP_BASE_NAME || '/weather-app';
+
     /**
      * encodeURIComponent() : URL 에서 안전하게 사용할 수 있도록 문자열을 인코딩 해준다.
      * 공백 %20
      */
-
+    // console.log(address);
+    
     if (address) {
         const geoUrl = isKisangcheongTest 
             ? `${baseName}/kisangcheong-test/api/naver?query=${encodeURIComponent(address)}` 
             : `${baseName}/api/naver?query=${encodeURIComponent(address)}`;
+
+            // console.log(geoUrl);
             const response = await fetch(geoUrl, {
                 method: 'GET',
                 headers: {
@@ -47,20 +51,22 @@ export const fetchWeatherData = async (baseDate, baseTime, nx, ny, isKisangcheon
     if (baseDate && baseTime && nx && ny){
         
         const weatherUrl = isKisangcheongTest 
-        ? `${baseName}/kisangcheong-test/api/weather/?base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`
-        : `${baseName}/api/weather/?base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`;
-    
-    const response = await fetch(weatherUrl, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-        },
-    });
+        ? `${baseName}/kisangcheong-test/api/weather?base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`
+        : `${baseName}/api/weather?base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`;
+        
+        console.log("weather url : ", weatherUrl);
+        
+        const response = await fetch(weatherUrl, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            },
+        });
 
-    if (!response.ok) {
-        throw new Error('Failed to fetch weather data');
-    }
+        if (!response.ok) {
+            throw new Error(`Failed to fetch weather data ${response.text()}`);
+        }
 
-    return response.json();
+        return response.json();
     }
 };
