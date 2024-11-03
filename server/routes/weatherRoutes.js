cac2d1c5d8159682ac292ddc70870350b2b4279f
juyeong <yp9782@gmail.com>
2024-11-03 16:59:24 +0900

    단기예보, 단기 실황 코드 겹쳐서 핸들러 구분

--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { weatherApiHandler } from '../controllers/weatherController.js';
+import { realTimeWeatherApiHandler,forecastWeatherApiHandler } from '../controllers/weatherController.js';
 
 const router = express.Router();
 
+router.get('/real-time', realTimeWeatherApiHandler); // 실시간 날씨
+router.get('/forecast', forecastWeatherApiHandler); // 예보 데이터
+
+/**
 router.get('/', weatherApiHandler);
 router.get('/kisangcheong-test', weatherApiHandler);
+
+ * 
+ */
 
 export default router;
