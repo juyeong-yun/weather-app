5cef53b851cf16be94a38ef47227a734cfc2b472
juyeong <yp9782@gmail.com>
2024-11-04 14:30:08 +0900

    icon 그림 넣기 완료

--- weather-app/src/utils/getIcons.js
+++ weather-app/src/utils/getIcons.js
@@ -1,16 +1,29 @@
 //  ./utils/getIcons.js
 import {getSkyType} from './codeChanging';
 
-export const getWeatherIcon = async(fcstValue) => {
+import sunny from '../images/sunny.png';
+import cloudy from '../images/cloudy.png';
+import fog from '../images/fog.png';
+
+const iconMapper = {
+    '맑음': {
+        icon: sunny,
+        alt: '맑은 날씨'
+    },
+    '구름 많음': {
+        icon: cloudy,
+        alt: '구름 많은 날씨'
+    },
+    '흐림': {
+        icon: fog,
+        alt: '흐린 날씨'
+    },
+};
+
+// 상태에 따른 아이콘과 대체 텍스트 반환 함수
+export const getWeatherIcon = (fcstValue) =>{
     const state = getSkyType(fcstValue);
 
-    if(state === '맑음') {
-        return "../imges/sunny.png";
-    } else if (state === '구름 많음'){
-        return "../imges/cloudy.png"
-    } else if (state === '흐림'){
-        return "../imges/fog.png"
-    } else {
-        
-    }
+    // iconMapper[state] : 현재 상태(state)에 해당하는 값을 찾아 반환
+    return iconMapper[state] || {icon:'', alt:''};
 };
